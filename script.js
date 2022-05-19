95dceb879e9e1e6dce4ab6c33e4dc7065b55baac
Jonas Barros <jonasfelipester@gmail.com>
2022-05-19 20:09:46 -0300

    Tentativa de implementação de logica 1 sem testes

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 console.log("Carregar....");
 function ValidaCPF(valor){
     console.log("VALIDA CPF");
-    return false;
-}
+    var validador1 =0;
+    for(let i=0;i<valor.length-2;i++){
+        validador1=validador1+(valor(i)*(10-i));
+    }
+    validador1=(validador1*10)%11;
+    var validador2 =0;
+    for(let i=0;i<valor.length-1;i++){
+        validador2=validador2+(valor(i)*(11-i));
+    }  
+    validador2=(validador2*10)%11;
+
+    
+    if((valor.length==11)&&(validador1=valor(9))&&(validador2=valor(10))){
+        return true;
+    }else{
+        return false;
+    }}
+    
+    
 
 function obterDados(){
     console.log("...Iniciando Validação...");//Testando se entrou na função
