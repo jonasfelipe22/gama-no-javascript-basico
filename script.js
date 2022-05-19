3db9fd884de802238f6470725fcb5ecf36570dad
Jonas Barros <jonasfelipester@gmail.com>
2022-05-19 20:39:57 -0300

    Tentativa de implementação de logica 3 erro de sintaxe linha37 chamando a função sem parametro e vetor em java se usa chaves[]

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 console.log("Carregar....");
+
+
 function ValidaCPF(valor){
     console.log("VALIDA CPF");
     var validador1 =0;
     for(let i=0;i<valor.length-2;i++){
-        validador1=validador1+(valor(i)*(10-i));
+        validador1=validador1+(valor[i]*(10-i));
     }
     validador1=(validador1*10)%11;
-    console.log("Validador1"+validador1);
+    console.log("Validador1 "+validador1);
     var validador2 =0;
     for(let i=0;i<valor.length-1;i++){
-        validador2=validador2+(valor(i)*(11-i));
+        validador2=validador2+(valor[i]*(11-i));
     }  
     validador2=(validador2*10)%11;
-    console.log("Validador2"+validador2);
-    console.log("Tamanho"+valor.length);
-    if((valor.length==11)&&(validador1=valor(9))&&(validador2=valor(10))){
+    console.log("Validador2 "+validador2);
+    console.log("Tamanho "+valor.length);
+    console.log("9 "+valor[9]);
+    console.log("10 "+valor[10]);
+
+    if((valor.length == 11) && (validador1 == valor[9]) && (validador2==valor[10])){
+        console.log("TRUE");
         return true;
-    }else{
+    } else {
         return false;
     }}
     
@@ -24,11 +30,11 @@ function ValidaCPF(valor){
 
 function obterDados(){
     console.log("...Iniciando Validação...");//Testando se entrou na função
-    var valor = document.getElementById("digitado").value;
+    let valor = document.getElementById("digitado").value;
     console.log(valor);
     ValidaCPF(valor);
-
-    if (ValidaCPF == true ){
+    console.log( ValidaCPF(valor));
+    if (ValidaCPF(valor)){
         document.getElementById("success").style.display = "block";
         console.log("success");
                             } 
