12ce856ba395888fed1544be9fdb36307ba81539
Jonas Barros <jonasfelipester@gmail.com>
2022-05-18 22:20:31 -0300

    obtendo valor do text box

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("Carregar....");
 function validacao_CPF(){
     console.log("...Iniciando Validação...");//Testando se entrou na função
-
+    var CPF = document.getElementById("CPF_dig").value;
+    console.log(CPF);
 }
